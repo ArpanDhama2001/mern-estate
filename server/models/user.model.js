724669e19e3b4547007caee2f0ffd7a28075ff9a
ArpanDhama2001 <arpandhama2001@gmail.com>
2023-10-13 23:32:42 +0530

    create user model

---
+++ server/models/user.model.js
@@ -0,0 +1,23 @@
+// create schema and model for the user
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    undefined: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
