9c293947a9bf52b285806e913f57aeed4777984f
ArpanDhama2001 <arpandhama2001@gmail.com>
2023-10-13 23:13:53 +0530

    connect mongodb, add .env

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import {BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from '../src/pages/Home'
 import Profile from '../src/pages/Profile'
--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGOOSE)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => console.log(err));
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
