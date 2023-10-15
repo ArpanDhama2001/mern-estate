598864bbce38226211c503b16c8d0d48a86bb17c
ArpanDhama2001 <arpandhama2001@gmail.com>
2023-10-15 13:30:24 +0530

    create sign-in and sign-up page ui

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
-  )
-}
+    <div className="my-5 max-w-lg mx-auto p-3">
+      <h1 className="text-3xl text-center font-semibold">Sign In</h1>
+      <form className="my-5 flex flex-col gap-4">
+        <input
+          type="email"
+          placeholder="email"
+          className="border p-3 rounded-lg w-full"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border p-3 rounded-lg w-full"
+        />
+        <button className="w-full bg-slate-700 uppercase text-white p-3 rounded-lg">
+          Sign In
+        </button>
+      </form>
+      <div className="flex gap-3">
+        <p className="">{`Don't have an account?`}</p>
+        <Link to="/signup">
+          <span className="uppercase text-blue-700">Sign Up</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default SignIn
+export default SignIn;
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
-  )
-}
+    <div className="my-5 max-w-lg mx-auto p-3">
+      <h1 className="text-3xl text-center font-semibold">Sign Up</h1>
+      <form className="my-5 flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="username"
+          className="border p-3 rounded-lg w-full"
+        />
+        <input
+          type="email"
+          placeholder="email"
+          className="border p-3 rounded-lg w-full"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border p-3 rounded-lg w-full"
+        />
+        <button className="w-full bg-slate-700 uppercase text-white p-3 rounded-lg">
+          Sign Up
+        </button>
+      </form>
+      <div className="flex gap-3">
+        <p className="">Already have an account?</p>
+        <Link to="/signin">
+          <span className="uppercase text-blue-700">Sign In</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default SignUp
+export default SignUp;
