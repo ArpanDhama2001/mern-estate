36fcc069545b8bebb6fc31bae1957ade529c369c
ArpanDhama2001 <arpandhama2001@gmail.com>
2023-10-15 20:54:48 +0530

    add signup functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,27 +1,72 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignUp = () => {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+      setError(null);
+      setLoading(false);
+      navigate("/signin");
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
   return (
     <div className="my-5 max-w-lg mx-auto p-3">
       <h1 className="text-3xl text-center font-semibold">Sign Up</h1>
-      <form className="my-5 flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="my-5 flex flex-col gap-4">
         <input
           type="text"
           placeholder="username"
           className="border p-3 rounded-lg w-full"
+          id="username"
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="email"
           className="border p-3 rounded-lg w-full"
+          id="email"
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="password"
           className="border p-3 rounded-lg w-full"
+          id="password"
+          onChange={handleChange}
         />
-        <button className="w-full bg-slate-700 uppercase text-white p-3 rounded-lg">
-          Sign Up
+        <button
+          disabled={loading}
+          className="w-full bg-slate-700 uppercase text-white p-3 rounded-lg disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign Up"}
         </button>
       </form>
       <div className="flex gap-3">
@@ -30,6 +75,7 @@ const SignUp = () => {
           <span className="uppercase text-blue-700">Sign In</span>
         </Link>
       </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
 };
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema(
     username: {
       type: String,
       required: true,
-      undefined: true,
+      unique: true,
     },
     email: {
       type: String,
